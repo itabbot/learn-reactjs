f01020b7fd6875e16d65827fbb7c4db9237a5b61
itabbot <itabbot@qq.com>
2023-09-24 19:33:57 +0800

    入口文件。

--- on-demand/react-router/contact-management/src/main.tsx
+++ on-demand/react-router/contact-management/src/main.tsx
@@ -1,50 +1,49 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {createBrowserRouter, RouterProvider,} from "react-router-dom";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import './main.css'
 
-import Root, {loader as rootLoader, action as rootAction,} from "./routes/root";
-import Contact, {loader as contactLoader, action as contactAction,} from "./routes/contact";
-import ErrorPage from "./error-page";
-import EditContact, {action as editAction,} from "./routes/edit";
-import {action as destroyAction} from "./routes/destroy";
-import Index from "./routes/index";
+// 路由组件
+import Index from "./common";
+import ErrorPage from "./common/error-page.tsx";
+import {Root, loader as rootLoader, action as rootAction} from "./common/root";
+import {ContactDetail, loader as contactLoader, action as contactAction} from "./contact/detail";
+import {EditContact, action as editAction} from "./contact/edit";
+import {action as destroyAction} from "./contact/destroy";
 
-import './index.css'
-
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Root/>,
-        errorElement: <ErrorPage/>,
-        loader: rootLoader,
-        action: rootAction,
-        children: [
-            {
-                errorElement: <ErrorPage/>,
-                children: [
-                    {index: true, element: <Index/>},
-                    {
-                        path: "contacts/:contactId",
-                        element: <Contact/>,
-                        loader: contactLoader,
-                        action: contactAction,
-                    },
-                    {
-                        path: "contacts/:contactId/edit",
-                        element: <EditContact/>,
-                        loader: contactLoader,
-                        action: editAction,
-                    },
-                    {
-                        path: "contacts/:contactId/destroy",
-                        action: destroyAction,
-                        errorElement: <div>Oops! There was an error.</div>,
-                    },
-                ],
-            },
-        ],
-    },
-]);
+// 创建路由器
+const router = createBrowserRouter([{
+    path: "/",
+    element: <Root/>,
+    errorElement: <ErrorPage/>,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+        {
+            errorElement: <ErrorPage/>,
+            children: [
+                {index: true, element: <Index/>},
+                {
+                    path: "contacts/:contactId",
+                    element: <ContactDetail/>,
+                    loader: contactLoader,
+                    action: contactAction,
+                },
+                {
+                    path: "contacts/:contactId/edit",
+                    element: <EditContact/>,
+                    loader: contactLoader,
+                    action: editAction,
+                },
+                {
+                    path: "contacts/:contactId/destroy",
+                    action: destroyAction,
+                    errorElement: <div>删除失败！</div>,
+                },
+            ],
+        },
+    ],
+}]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
